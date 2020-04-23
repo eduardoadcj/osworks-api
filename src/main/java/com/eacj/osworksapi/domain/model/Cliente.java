5246928d141139defcc4ca55fc332d885480f683
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-04-23 17:04:22 -0300

    mapeamento da classe

--- src/main/java/com/eacj/osworksapi/domain/model/Cliente.java
+++ src/main/java/com/eacj/osworksapi/domain/model/Cliente.java
@@ -1,8 +1,18 @@
 package com.eacj.osworksapi.domain.model;
 
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY é a utilização da forma nativa definida no DDL do banco
     private Long id;
+    
     private String nome;
     private String email;
     private String telefone;
@@ -37,6 +47,31 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
     }
     
 }
